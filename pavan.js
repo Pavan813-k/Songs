7d42c3994a3f197638f6d790e8fc3d12b2c698a7
Pavan813-k <pavangonna813@gmail.com>
2025-05-15 14:10:39 +0530

    Add files via upload

---
+++ pavan.js
@@ -0,0 +1,10 @@
+function playAudio() {
+    var audio = new Audio('Hungry Cheetah.mp3');
+    audio.play();
+}
+function stopAudio() {
+    var audio = new Audio('Hungry Cheetah.mp3');
+    audio.pause();
+    audio.currentTime = ;
+}
+
